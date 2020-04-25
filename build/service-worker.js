39c27c00b09fdce404cdbadc1646ff2a5e79730e
Connor Mote <connor27mote@gmail.com>
2020-04-25 15:16:36 -0600

    Added padding

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Portfolio-Website-React/precache-manifest.18005d34d6bcd2b7fe2c019235e9e7df.js"
+  "/Portfolio-Website-React/precache-manifest.ca2be4a1ced034da7a54bb80f34b9153.js"
 );
 
 self.addEventListener('message', (event) => {
