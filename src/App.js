fa88f4126235d9ac6f50ad5753852959dc42bc2a
Connor Mote <connor27mote@gmail.com>
2020-04-26 13:33:33 -0600

    Changed title

--- src/App.js
+++ src/App.js
@@ -1,18 +1,22 @@
 import React, { useState, useEffect } from 'react';
-import Header from './components/layout/Header';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import {
-  faGithub,
-  faLinkedin,
-  faFacebook,
-} from '@fortawesome/free-brands-svg-icons';
 import './css/main.css';
-import Routing from './components/layout/routing';
+//import Routing from './components/layout/routing';
+import { HashRouter, Switch, Route } from 'react-router-dom';
+import AboutMe from './components/pages/AboutMe';
+import MyWork from './components/pages/MyWork';
+import HowToReachMe from './components/pages/HowToReachMe';
+import LandingPage from './components/pages/LandingPage';
 
 function App() {
   return (
-    //<Router> - alias for BrowserRouter imported above. exact prevents main page from allways showing
-    <Routing />
+    <HashRouter>
+      <Switch>
+        <Route exact path="/" component={LandingPage} />
+        <Route path="/AboutMe" component={AboutMe} />
+        <Route path="/MyWork" component={MyWork} />
+        <Route path="/HowToReachMe" component={HowToReachMe} />
+      </Switch>
+    </HashRouter>
   );
 }
 
